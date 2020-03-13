7c42b56ccd18c81bdb4ba57926787026df8ff9d4
1100261 <spakhc@11stcorp.com>
2020-03-13 11:05:49 +0900

    init

---
+++ src/main/java/com/aws/hcaws/HcawsApplication.java
@@ -0,0 +1,13 @@
+package com.aws.hcaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcawsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HcawsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aws/hcaws/HcawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.hcaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcawsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
